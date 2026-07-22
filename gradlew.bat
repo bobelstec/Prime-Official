ef901da96ef7e5094a60ba02d9032d475f7e160d
bobelstec <138162010+bobelstec@users.noreply.github.com>
2026-07-22 15:25:10 -0300

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-where gradle >nul 2>nul
-if %ERRORLEVEL%==0 (
-  gradle %*
-) else (
-  echo Gradle nao encontrado. Abra no Android Studio ou use o GitHub Actions.
-  echo Veja COMO_GERAR_O_APK.md.
-  exit /b 1
-)
